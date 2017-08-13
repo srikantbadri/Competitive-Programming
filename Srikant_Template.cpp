a7d9f1b8759a97b7ebb54d4091b35f586e86b64f
Srikant Badri <srikantbadri96@gmail.com>
2017-08-13 07:02:23 +0530

    Template which is use in contests

---
+++ Srikant_Template.cpp
@@ -0,0 +1,43 @@
+//Coder- Srikant Badri :)
+#include<bits/stdc++.h>
+using namespace std; 
+typedef pair<int,int> ii; 
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long int l;
+typedef double d;
+typedef vector<ll> vi;
+typedef vector<vi> vvi; 
+
+#define de(a)			scanf("%lld",&a);
+#define de2(a,b)		scanf("%lld %lld %lld",&a,&b,&c);
+#define de3(a,b,c)		scanf("%lld %lld %lld",&a,&b,&c);
+#define le(a)			printf("%lld\n",a);
+#define p(e)			printf("\n");
+#define loop(i,a)		for(long long int i=0;i<a;i++)
+#define loop2(i,a,b)	for(long long int i=a;i<b;i++)
+#define fill(a,x)		memset(a,x,sizeof(a))
+#define debug(ccc)		cout<<#ccc<<"="<<ccc<<endl;
+#define sz(a) 			int((a).size()) 
+#define pb 				push_back 
+#define all(c) 			(c).begin(),(c).end() 
+#define tr(c,i) 		for(typeof((c).begin()) i = (c).begin(); i != (c).end(); i++) 
+#define present(c,x) 	((c).find(x) != (c).end()) 
+#define cpresent(c,x) 	(find(all(c),x) != (c).end())
+#define FastRead 		ios_base::sync_with_stdio(0);cin.tie(0); 
+
+const double eps=1e-8;
+const int mod=1e9+7;
+//const ll inf=1e18;
+#define EQ(a,b)	(fabs((a)-(b))<eps)
+int main()
+{
+	ll t,i;
+	//freopen("C://Users//Badri//Desktop//input.txt", "r", stdin);
+    //freopen("C://Users//Badri//Desktop//output.txt", "w", stdout);*/
+	de(t);
+	while(t--)
+	{
+	}
+	return 0;
+}
